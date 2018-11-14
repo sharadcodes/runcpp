5f122196a2c1cd971cadd3bb0cad7ce76e1beca0
SHARAD RAJ SINGH MAURYA <36638057+sharadcodes@users.noreply.github.com>
2018-11-14 22:16:46 +0530

    Add files via upload

--- runcpp.bat
+++ runcpp.bat
@@ -4,30 +4,36 @@ ECHO. ##################################################################
 ECHO. ###################### C++ COMPILER HELPER #######################
 ECHO. ################# Developer: github @sharadcodes #################
 ECHO.
+
+if %~x1%==.c (
+echo. ------------------------- It's a C File -------------------------- )
+if %~x1%==.cpp (
+echo. ------------------------- It's a C++ File ------------------------ )
+if %~x1%==.py (
+echo. ----------------------- It's a Python File ----------------------- )
+
+ECHO.
 ECHO. ############################ OUTPUT ##############################
+
 ECHO.
 ECHO.
 
 if %~x1%==.cpp (
-echo. It's a C++ File
 g++ %~f1 -o %~n1.exe 
 %~n1.exe
-TYPE %~f1 > %~n1_pro.txt
-%~n1.exe > %~n1.txt
 )
 
 if %~x1%==.c (
-echo. It's a C File
 gcc %~f1 -o %~n1.exe 
 %~n1.exe
-TYPE %~f1 > %~n1_pro.txt
-%~n1.exe > %~n1.txt
 )
 
-%~n1.exe > %~n1.txt
+if %~x1%==.py ( 
+python %~f1 
+) 
+
 ECHO.
 ECHO.
 ECHO.
 ECHO.
-pause 100
-cmd
+pause
