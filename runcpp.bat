1286cb8319d4c49adfeb1f97902085fc2a3eb58e
SHARAD RAJ SINGH MAURYA <36638057+sharadcodes@users.noreply.github.com>
2018-11-14 21:35:03 +0530

    Add files via upload

--- runcpp.bat
+++ runcpp.bat
@@ -5,13 +5,29 @@ ECHO. ###################### C++ COMPILER HELPER #######################
 ECHO. ################# Developer: github @sharadcodes #################
 ECHO.
 ECHO. ############################ OUTPUT ##############################
-REM g++ %~nx1 -o %~n1
 ECHO.
 ECHO.
-g++ %~f1 -o %~n1
+
+if %~x1%==.cpp (
+echo. It's a C++ File
+g++ %~f1 -o %~n1.exe 
 %~n1.exe
+TYPE %~f1 > %~n1_pro.txt
+%~n1.exe > %~n1.txt
+)
+
+if %~x1%==.c (
+echo. It's a C File
+gcc %~f1 -o %~n1.exe 
+%~n1.exe
+TYPE %~f1 > %~n1_pro.txt
+%~n1.exe > %~n1.txt
+)
+
+%~n1.exe > %~n1.txt
 ECHO.
 ECHO.
 ECHO.
 ECHO.
-pause
+pause 100
+cmd
