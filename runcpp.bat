a9a627d839a1ad5b96a12a1c742c5654e1e62bac
SHARAD RAJ SINGH MAURYA <36638057+sharadcodes@users.noreply.github.com>
2018-11-15 01:58:58 +0530

    Update runcpp.bat

--- runcpp.bat
+++ runcpp.bat
@@ -6,26 +6,39 @@ ECHO. ################# Developer: github @sharadcodes #################
 ECHO.
 
 if %~x1%==.c (
-echo. ------------------------- It's a C File -------------------------- )
+ECHO. ------------------------- It's a C File -------------------------- )
 if %~x1%==.cpp (
-echo. ------------------------- It's a C++ File ------------------------ )
+ECHO. ------------------------- It's a C++ File ------------------------ )
 if %~x1%==.py (
-echo. ----------------------- It's a Python File ----------------------- )
+ECHO. ----------------------- It's a Python File ----------------------- )
+
+ECHO. Source file name: %~n1
+ECHO. Source path     : %~f1
+ECHO. Source binary   : %~dp1%~n1.exe
 
 ECHO.
 ECHO. ############################ OUTPUT ##############################
-
 ECHO.
 ECHO.
 
 if %~x1%==.cpp (
-g++ %~f1 -o %~n1.exe 
-%~n1.exe
+REM g++ %~f1 -o %~n1.exe 
+REM g++ %~n1.cpp -o %~n1.exe last
+g++ %~f1 -o %~dp1%~n1.exe
+%~dp1%~n1.exe
+ECHO.
+REM del %~n1.exe
+del %~dp1%~n1.exe
 )
 
 if %~x1%==.c (
-gcc %~f1 -o %~n1.exe 
-%~n1.exe
+REM gcc %~f1 -o %~n1.exe 
+REM gcc %~n1.cpp -o %~n1.exe  last
+gcc %~n1.cpp -o %~dp1%~n1.exe
+%~dp1%~n1.exe
+ECHO.
+REM del %~n1.exe
+del %~dp1%~n1.exe
 )
 
 if %~x1%==.py ( 
